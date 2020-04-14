561fd6b8c51dd01daea475b958df03df40c63fec
Marian Angelov <50790794+marianangelov@users.noreply.github.com>
2020-04-14 19:49:12 +0300

    Grid problem

---
+++ grid/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Grid from './grid/Grid';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <Grid />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
